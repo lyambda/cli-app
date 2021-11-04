7adc2b9e3b745321d62ab009846f2597c8297094
Всеволод <63463168+vsecoder@users.noreply.github.com>
2021-11-04 12:14:57 +0300

    Убрал заглушки, и на их место рабочий код

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-import requests
+import requests, json
 
-token = ''
 auth = False
 email = ''
 link = 'https://lyabmda12345.herokuapp.com/'
 
+def config():
+    f = open(f'config.json', "r")
+    settings = json.loads(f.read())
+    token = settings["token"]
+    return token
+
 def request(method, t_bool, args):
-    global token
+    token = config()
     res = ''
     if t_bool:
-        res = requests.get(f'{link}/{method}?token={t_bool}&{args}')
+        res = requests.get(f'{link}/{method}?token={token}&{args}')
     else:
         res = requests.get(f'{link}/{method}?{args}')
     if res.status_code == 200:
@@ -29,55 +34,82 @@ class Auth():
     def join(code):
         res = request('signIn', False, f'email={email}&code={code}')
         token = res['token']
+        print(token)
+        f = open(f'config.json', "w")
+        f.write('''{"token": "''' + token + '''"}''')
         if (res['is_auth']):  # login
             pass
         else:       # register
             Auth.register(token)
         return token
 
-    def register(token):
-        name = input("Name >>> ")
-        surname = input("Surname >>> ")
-        description = input("Description >>> ")
-        res = request('register', token, f'name={name}&surname={surname}&description={description}&code={code}')
+    def register():
+        name = input(" Name >>> ")
+        surname = input(" Surname >>> ")
+        description = input(" Description >>> ")
+        res = request('register', True, f'name={name}&surname={surname}&description={description}&code={code}')
 
-    def logout(token):
-        res = request('logOut', token, '')
+    def logout():
+        res = request('logOut', True, '')
+        f = open(f'config.json', "w")
+        f.write('''{"token": ""}''')
         raise Exception('You loged out!')
 
 class Group():
-    def createGroup(name, link, description):
-        #res = request('createGroup', True, f'')
-        pass
+    def createGroup():
+        name = input(" Name >>> ")
+        link = input(" Link >>> ")
+        description = input(" Description >>> ")
+        res = request('createGroup', True, f'name={name}&link={link}&description={description}')
+        return res
 
-    def joinGroup(token, id_group):
-        #res = request('joinGroup', True, f'')
-        pass
+    def joinGroup():
+        id_group = input(" Group ID >>> ")
+        res = request('joinGroup', True, f'id_group={id_group}')
+        return res
 
-    def sendMessage(token, id_group, text):
-        #res = request('sendMessage', True, f'')
-        pass
+    def sendMessage():
+        id_group = input(" Group ID >>> ")
+        text = input(" Text >>> ")
+        res = request('sendMessage', True, f'id_group={id_group}&text={text}')
+        return res
 
-    def getMessages(token, id_group):
-        #res = request('getMessages', True, f'')
-        pass
+    def getMessages():
+        id_group = input(" Group ID >>> ")
+        res = request('getMessages', True, f'id_group={id_group}')
+        return res
 
 class Profile():
     def _me():
-        res = requests.get(f'{link}/me?session_token={token}').json()
+        res = res = request('me', True, f'')
         return res
 
 class InputBox():
     def funcs(text):
-        pass
-
-email = input('Email >>> ')
-Auth.sendCode(email)
-code = input('Code >>> ')
-token = Auth.join(code)
-
-# ...
+        cmds = {
+            'createGroup': {'func': Group.createGroup, 'args': 3},
+            'joinGroup': {'func': Group.joinGroup, 'args': 1},
+            'sendMessage': {'func': Group.sendMessage, 'args': 2},
+            'getMessages': {'func': Group.getMessages, 'args': 1},
+            'me': {'func': Profile._me, 'args': 0},
+            'logOut': {'func': Auth.logout, 'args': 0}
+        }
+        try:
+            text = text.split()
+            if text[0] in cmds:
+                cmd = cmds.get(text[0])
+                print(cmd['func']())
+                
+        except Exception as e:
+            print(f"Error: {e}")
 
 while True:
-    text = input("Input: ")
-    InputBox.funcs(text)
+    if config():
+        while True:
+            text = input("Input: ")
+            InputBox.funcs(text)
+    else:
+        email = input('Email >>> ')
+        Auth.sendCode(email)
+        code = input('Code >>> ')
+        token = Auth.join(code)
